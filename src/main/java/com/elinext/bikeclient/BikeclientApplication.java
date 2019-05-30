378a335a8ccde1f85e1235325a6c17c664439b71
Pavel Radkevich <pradkevich@elinext.com>
2019-05-30 16:49:53 +0300

    initial commit

---
+++ src/main/java/com/elinext/bikeclient/BikeclientApplication.java
@@ -0,0 +1,13 @@
+package com.elinext.bikeclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeclientApplication.class, args);
+	}
+
+}
