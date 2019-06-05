5167a85a9ab7e3000ed9f6c02515456cddebf59a
Pavel Radkevich <pradkevich@elinext.com>
2019-06-05 15:52:09 +0300

    working demo with roles

--- src/main/java/com/elinext/bikeclient/security/WebConfig.java
+++ src/main/java/com/elinext/bikeclient/security/WebConfig.java
@@ -3,6 +3,7 @@ package com.elinext.bikeclient.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class WebConfig extends WebSecurityConfigurerAdapter {
